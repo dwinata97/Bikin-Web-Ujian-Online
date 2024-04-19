377fa7c83bc699e0fe7a0359a133d27254bae196
dwinata97 <167524292+dwinata97@users.noreply.github.com>
2024-04-19 17:31:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+//buat koneksi
+$koneksi = mysqli_connect("localhost","root","","db_smp_n1ujm");
+
+//cek koneksi
+//if (mysqli_connect_errno()) {
+//    echo "koneksi gagal. Cek Lagi";
+//} else {
+//    echo "Koneksi Berhasil. Lanjut";
+//}
+
+//url induk
+$main_url = "http://localhost/smpn1ujm";
